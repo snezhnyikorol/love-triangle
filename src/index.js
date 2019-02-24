2605b366743813047750b97e26b1ccba9d0bcfe2
Aliaksei Raketski <raketskij@gmail.com>
2019-02-24 20:18:58 +0300

    Commit

--- src/index.js
+++ src/index.js
@@ -3,5 +3,13 @@
  * @returns number of love triangles
  */
 module.exports = function getLoveTrianglesCount(preferences = []) {
-  // your implementation
+  var count = 0;
+  for (let i = 0; i < preferences.length; i++) {
+    if ((preferences[i] != 0) & (preferences[i] != i + 1)) {
+      if (preferences[preferences[preferences[i]-1]-1]-1 == i) {
+      count++;
+      }
+    }
+  }
+  return count / 3;
 };
